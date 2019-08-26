012d4391910250900a2a6280b7ea7ef241dc8728
kabramovich <wieker@gmail.com>
2019-08-26 23:38:08 +0300

    LPCXpresso LPC4367 OM13088UL: SGPIO: SPI CS# as qualifier

--- firmware/liblpc/sgpio.c
+++ firmware/liblpc/sgpio.c
@@ -35,6 +35,7 @@ static void con_print_data(const uint8_t *dat, int sz)
 
 void sgpio_main() {
     DEBUGOUT("SGPIO init entry\r\n");
+    Chip_SCU_PinMuxSet(1, 2, (SCU_PINIO_FAST |  SCU_MODE_FUNC3));
     Chip_SCU_PinMuxSet(1, 3, (SCU_PINIO_FAST |  SCU_MODE_FUNC2));
     Chip_SCU_PinMuxSet(1, 4, (SCU_PINIO_FAST |  SCU_MODE_FUNC2));
 
@@ -49,15 +50,17 @@ void sgpio_main() {
             (0L << 12) |    // CONCAT_ORDER = 0 (self-loop)
             (0L << 11) |    // CONCAT_ENABLE = 1 (concatenate data)
             (0L <<  9) |    // QUALIFIER_SLICE_MODE = X
-            (0L <<  7) |    // QUALIFIER_PIN_MODE = X
-            (0L <<  5) |    // QUALIFIER_MODE = 0 (enable)
+            (0L <<  8) |    // QUALIFIER_PIN_MODE = X
+            (1L <<  7) |    // QUALIFIER_PIN_MODE = X
+            (1L <<  6) |    // QUALIFIER_MODE = 0 (enable)
+            (1L <<  5) |    // QUALIFIER_MODE = 0 (enable)
             (0L <<  3) |    // CLK_SOURCE_SLICE_MODE = 0, slice D
             (1L <<  2) |    // CLK_SOURCE_PIN_MODE = X
             (1L <<  1) |    // CLK_SOURCE_PIN_MODE = X
             (1L <<  0);     // EXT_CLK_ENABLE = 0, internal clock signal (slice)
 
     LPC_SGPIO->SLICE_MUX_CFG[6] =
-            (0L <<  8) |    // INV_QUALIFIER = 0 (use normal qualifier)
+            (1L <<  8) |    // INV_QUALIFIER = 0 (use normal qualifier)
             (0L <<  6) |    // PARALLEL_MODE = 0 (shift 1 bit per clock)
             (0L <<  4) |    // DATA_CAPTURE_MODE = 0 (detect rising edge)
             (0L <<  3) |    // INV_OUT_CLK = 0 (normal clock)
