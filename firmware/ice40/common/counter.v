893ed5bff735baf6c5214ca50f832ffae94e42da
kabramovich <wieker@gmail.com>
2019-09-09 02:07:19 +0300

    FPGA counter

--- firmware/ice40/common/counter.v
+++ firmware/ice40/common/counter.v
@@ -1,8 +1,7 @@
-module counter(input wire clk, input wire[7:0] value, input restart,
+module counter(input wire clk, input wire[15:0] value, input restart,
     output wire out);
-    reg[7:0] counter_reg;
+    reg[15:0] counter_reg;
     reg out_reg;
-    reg keep;
 
     always @(posedge clk)
     begin
@@ -12,19 +11,11 @@ module counter(input wire clk, input wire[7:0] value, input restart,
                     begin
                         counter_reg <= 0;
                         out_reg <= ~out_reg;
-                        keep <= 0;
                     end
                 else
                     begin
-                        if (~keep) begin
-                            counter_reg <= counter_reg;
-                            out_reg <= ~out_reg;
-                            keep <= 1;
-                        end else begin
-                            counter_reg <= counter_reg;
-                            out_reg <= out_reg;
-                            keep <= keep;
-                        end
+                        counter_reg <= counter_reg;
+                        out_reg <= 1;
                     end
             end
         else
