6539956cd062b14a69c6cd4096757091f8b3864a
kabramovich <wieker@gmail.com>
2019-09-21 16:43:04 +0300

    Programming iCE40-1KHX EVB through ice40UP5K EVB (pass SCK line)

--- firmware/ice40/spipass/top.v
+++ firmware/ice40/spipass/top.v
@@ -1,10 +1,7 @@
 module top(
-    output ICE_SS, ICE_SCK, ICE_MOSI, IOB_2A, input IOB_6A, IOB_0A, IOB_3B, ICE_MISO
+    output IOB_0A, input IOB_6A
 );
 
-    assign ICE_SS = IOB_6A;
-    assign ICE_SCK = IOB_0A;
-    assign ICE_MOSI = IOB_3B;
-    assign IOB_2A = ICE_MISO;
+    assign IOB_0A = IOB_6A;
 
 endmodule
