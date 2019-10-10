f1b08e844df149bcfcf0daba6ee8c952d32549aa
kabramovich <wieker@gmail.com>
2019-10-10 19:59:52 +0300

    ice40HX1K board: board assembled and tested. it works

--- firmware/ice40/hx1k/top.v
+++ firmware/ice40/hx1k/top.v
@@ -2,21 +2,10 @@
 `include "../common/counter.v"
 
 module top(
-    input  clk, output led1, led2, led3
+    input  clk, led1, output led2, led3
 );
 
-    reg [23:0] led1_reg;
-    reg [23:0] led2_reg;
-    reg [23:0] led3_reg;
-
-    always @(posedge clk) begin
-        led1_reg <= led1_reg + 1;
-        led2_reg <= led2_reg + 1;
-        led3_reg <= led3_reg + 1;
-    end
-
-    assign led1 = led1_reg[23];
-    assign led2 = ~led1_reg[21];
-    assign led3 = led1_reg[22];
+    assign led2 = led1;
+    assign led3 = clk;
 
 endmodule
