256339e0fd66b906a3af6951aa0506d732eb88c3
kabramovich <wieker@gmail.com>
2019-10-20 12:48:28 +0300

    HX1KADC board: 6502 => SRAM some blinks

--- firmware/ice40/hx1k-6502/src/tst_6502.v
+++ firmware/ice40/hx1k-6502/src/tst_6502.v
@@ -9,7 +9,12 @@ module tst_6502(
 	input [7:0] gpio_i,
 	
 	input RX,				// serial RX
-	output TX				// serial TX
+	output TX,				// serial TX
+
+	output sram_oe,
+    input [7:0] sram_din,
+    output [7:0] sram_dout,
+    output [15:0] addr,
 );
     // The 6502
     wire [15:0] CPU_AB;
@@ -33,57 +38,14 @@ module tst_6502(
 	wire p1 = (CPU_AB[15:12] == 4'h1) ? 1 : 0;
 	wire p2 = (CPU_AB[15:12] == 4'h2) ? 1 : 0;
 	wire pf = (CPU_AB[15:12] == 4'hf) ? 1 : 0;
-	
-	// RAM @ pages 00-0f
-	reg [7:0] ram_mem[4095:0];
-	reg [7:0] ram_do;
-	always @(posedge clk)
-		if((CPU_WE == 1'b1) && (p0 == 1'b1))
-			ram_mem[CPU_AB[11:0]] <= CPU_DO;
-	always @(posedge clk)
-		ram_do <= ram_mem[CPU_AB[11:0]];
-	
+
+	assign sram_oe = (CPU_WE == 1'b1) && (p0 == 1'b1);
+	assign addr = CPU_AB;
+	assign sram_dout = CPU_DO;
+	assign sram_din = CPU_DI;
+
 	// GPIO @ page 10-1f
 	reg [7:0] gpio_do;
 	always @(posedge clk)
-		if((CPU_WE == 1'b1) && (p1 == 1'b1))
-			gpio_o <= CPU_DO;
-	always @(posedge clk)
-		gpio_do <= gpio_i;
-	
-	// ACIA at page 20-2f
-	wire [7:0] acia_do;
-	acia uacia(
-		.clk(clk),				// system clock
-		.rst(reset),			// system reset
-		.cs(p2),				// chip select
-		.we(CPU_WE),			// write enable
-		.rs(CPU_AB[0]),			// register select
-		.rx(RX),				// serial receive
-		.din(CPU_DO),			// data bus input
-		.dout(acia_do),			// data bus output
-		.tx(TX),				// serial transmit
-		.irq(CPU_IRQ)			// interrupt request
-	);
-	
-	// ROM @ pages f0,f1...
-    reg [7:0] rom_mem[4095:0];
-	reg [7:0] rom_do;
-	initial
-        $readmemh("rom.hex",rom_mem);
-	always @(posedge clk)
-		rom_do <= rom_mem[CPU_AB[11:0]];
-
-	// data mux
-	reg [3:0] mux_sel;
-	always @(posedge clk)
-		mux_sel <= CPU_AB[15:12];
-	always @(*)
-		casez(mux_sel)
-			4'h0: CPU_DI = ram_do;
-			4'h1: CPU_DI = gpio_do;
-			4'h2: CPU_DI = acia_do;
-			4'hf: CPU_DI = rom_do;
-			default: CPU_DI = rom_do;
-		endcase
+	    gpio_o <= addr[0:7];
 endmodule
