5584ddce6158feaf9d562ca6c2f8a5aa2d94059a
kabramovich <wieker@gmail.com>
2019-10-20 17:16:29 +0300

    HX1KADC board: UART TX/RX

--- firmware/ice40/hx1k/top.v
+++ firmware/ice40/hx1k/top.v
@@ -10,6 +10,10 @@ module top(
     reg [20:0] counter;
     wire [21:0] next = counter + 1;
 
+    wire tmp;
+    reg [7:0] dt = 8'h3f;
+    wire [7:0] dtwire;
+    wire int;
 
     ringoscillator #(.DELAY_LUTS(200)) rng(clk);
 
@@ -18,13 +22,15 @@ module top(
     always @(posedge clk)
         begin
             counter <= next;
+            if (int)
+                dt <= dtwire;
         end
 
     assign led1 = next[20];
     assign led2 = next[19];
-    wire tmp;
 
-    uart_tx uart_tx(clk, 1, next[21], 8'h3f, tmp, led3);
+    uart_tx uart_tx(clk, 1, next[21], dt, tmp, led3);
+    uart_rx uart_rx(clk, 1, rx, int, dtwire);
     assign tx = tmp;
 
 endmodule
